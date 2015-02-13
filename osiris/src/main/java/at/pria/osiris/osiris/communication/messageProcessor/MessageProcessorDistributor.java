5d674e33260013e327ba7c6751247d6fea5b8f43
Ari Ayvazyan <ariay@live.at>
2015-02-13 17:15:14 +0100

    design improvements

--- osiris/src/main/java/at/pria/osiris/osiris/communication/messageProcessor/MessageProcessorDistributor.java
+++ osiris/src/main/java/at/pria/osiris/osiris/communication/messageProcessor/MessageProcessorDistributor.java
@@ -8,15 +8,19 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * Sends a message via socket
+ * Distributes messages to listeners.
  *
- * @author Adrian Bergler
+ * @author Ari Ayvazyan
  * @version 2014-10-17
  */
-public class MessageProcessorDistributor {
+public class MessageProcessorDistributor implements MessageProcessor{
 
     private List<MessageProcessor> messageProcessors;
 
+    /**
+     * Distributes messages over its listeners,
+     * the listeners need to find out by themselves if they have the correct object by calling instanceof.
+     */
     public MessageProcessorDistributor() {
         this.messageProcessors = new LinkedList<MessageProcessor>();
     }
@@ -30,9 +34,7 @@ public class MessageProcessorDistributor {
     public void processMessage(byte[] message) {
         try {
             Object receivedMessage = Serializer.deserialize(message);
-            for (MessageProcessor messageProcessor : messageProcessors) {
-                messageProcessor.processMessage(receivedMessage);
-            }
+            processMessage(receivedMessage);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
@@ -40,7 +42,18 @@ public class MessageProcessorDistributor {
         }
     }
 
+    /**
+     * Adds a MessageProcessor that receives messages.
+     * @param messageProcessor the MessageProcessor to be added.
+     */
     public void addMessageProcessor(MessageProcessor messageProcessor) {
         this.messageProcessors.add(messageProcessor);
+    }
+
+    @Override
+    public void processMessage(Object message) {
+        for (MessageProcessor messageProcessor : messageProcessors) {
+            messageProcessor.processMessage(message);
+        }
     }
 }
