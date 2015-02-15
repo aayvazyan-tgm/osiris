990414b4c57462ac9d324795033349f5375e2a5f
Ari Ayvazyan <ariay@live.at>
2015-02-16 00:58:25 +0100

    implemented the main function

--- linker/src/main/java/at/pria/osiris/linker/Main.java
+++ linker/src/main/java/at/pria/osiris/linker/Main.java
@@ -1,5 +1,9 @@
 package at.pria.osiris.linker;
 
+import at.pria.osiris.linker.communication.messageProcessors.MessageProcessorDistributor;
+import at.pria.osiris.linker.controllers.RobotArm;
+import at.pria.osiris.linker.implementation.hedgehog.HedgehogRobotArm;
+
 /**
  * Starts the Linker and sets up its communication.
  *
@@ -8,6 +12,15 @@ package at.pria.osiris.linker;
  */
 public class Main {
     public static void main(String[] args){
+        //Initialize a RobotArm Implementation
+        RobotArm robotArm=new HedgehogRobotArm();
+        //Initialize the MessageProcessorDistributor and add MessageProcessors to handle incoming requests
+        MessageProcessorDistributor msgDistributor= new MessageProcessorDistributor();
+        //Add the handlers
+        //TODO Add the handlers
+        msgDistributor.addMessageProcessor();
+        //Setup the Communication of the specified RobotArm/Controller
+        robotArm.getCommunicationInterface().setupCommunication(msgDistributor);
 
     }
 }
