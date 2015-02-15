2010ee8ffe569cfde2d4ec75db17c1cd96f444e4
Ari Ayvazyan <ariay@live.at>
2015-02-16 00:09:22 +0100

    initial commit

---
+++ linker/src/main/java/at/pria/osiris/linker/Main.java
@@ -0,0 +1,13 @@
+package at.pria.osiris.linker;
+
+/**
+ * Starts the Linker and sets up its communication.
+ *
+ * @author Ari Michael Ayvazyan
+ * @version 15.02.2015
+ */
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
