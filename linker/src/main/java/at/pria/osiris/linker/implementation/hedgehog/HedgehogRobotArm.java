d13d4cecf0dabb0cc6b72fc70317ea02649afe03
Ari Ayvazyan <ariay@live.at>
2015-02-16 01:56:24 +0100

    added stub methods

--- linker/src/main/java/at/pria/osiris/linker/implementation/hedgehog/HedgehogRobotArm.java
+++ linker/src/main/java/at/pria/osiris/linker/implementation/hedgehog/HedgehogRobotArm.java
@@ -5,12 +5,26 @@ import at.pria.osiris.linker.controllers.RobotArm;
 import at.pria.osiris.linker.controllers.components.Axis;
 import at.pria.osiris.linker.implementation.hedgehog.communication.HedgehogCommunicationInterface;
 
+/**
+ *
+ * @author Ari Michael Ayvazyan
+ * @version 15.02.2015
+ */
 public class HedgehogRobotArm extends RobotArm {
     private static HedgehogCommunicationInterface communicationInterfaceINSTANCE =new HedgehogCommunicationInterface();
+
+    public HedgehogRobotArm() {
+//TODO this class is a stub
+	}
 
     @Override
     public Axis[] getAvailableAxes() {
         return new Axis[0];
+    }
+
+    @Override
+    public Axis getAxis(int i) {
+        return null;
     }
 
     @Override
