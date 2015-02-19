26318d325efa963d338389b0b5de8ee196163d3e
Ari Ayvazyan <ariay@live.at>
2015-02-19 10:35:22 +0100

    implemented axes

--- linker/src/main/java/at/pria/osiris/linker/implementation/hedgehog/HedgehogRobotArm.java
+++ linker/src/main/java/at/pria/osiris/linker/implementation/hedgehog/HedgehogRobotArm.java
@@ -4,7 +4,13 @@ import at.pria.osiris.linker.communication.CommunicationInterface;
 import at.pria.osiris.linker.communication.messageProcessors.MessageProcessor;
 import at.pria.osiris.linker.controllers.RobotArm;
 import at.pria.osiris.linker.controllers.components.Axis;
+import at.pria.osiris.linker.implementation.hedgehog.axes.BaseAxis;
+import at.pria.osiris.linker.implementation.hedgehog.axes.HorizontalAxis;
+import at.pria.osiris.linker.implementation.hedgehog.axes.VerticalAxis;
 import at.pria.osiris.linker.implementation.hedgehog.communication.HedgehogCommunicationInterface;
+import org.andrix.low.NotConnectedException;
+
+import java.util.ArrayList;
 
 /**
  *
@@ -13,20 +19,39 @@ import at.pria.osiris.linker.implementation.hedgehog.communication.HedgehogCommu
  */
 public class HedgehogRobotArm extends RobotArm {
 
+    private final ArrayList<Axis> axes;
+
     public HedgehogRobotArm(MessageProcessor messageProcessor) {
         //Add the Communication Interface
         super(new HedgehogCommunicationInterface(),messageProcessor);
+        //Add the axes
+        this.axes=new ArrayList<Axis>();
+        try {
+            this.axes.add(new BaseAxis());
+        } catch (NotConnectedException e) {
+            e.printStackTrace();
+        }
+        try {
+            this.axes.add(new VerticalAxis());
+        } catch (NotConnectedException e) {
+            e.printStackTrace();
+        }
+        try {
+            this.axes.add(new HorizontalAxis());
+        } catch (NotConnectedException e) {
+            e.printStackTrace();
+        }
     }
 
 
     @Override
     public Axis[] getAvailableAxes() {
-        return new Axis[0];
+        return axes.toArray(new Axis[axes.size()]);
     }
 
     @Override
     public Axis getAxis(int i) {
-        return null;
+        return this.axes.get(i);
     }
 
 
