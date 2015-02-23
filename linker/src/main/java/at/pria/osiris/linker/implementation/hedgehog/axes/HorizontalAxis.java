301496f2acd4d46909da5e881425e635e4bf4871
Ari Ayvazyan <ariay@live.at>
2015-02-23 14:25:28 +0100

    implemented the new axis design

--- linker/src/main/java/at/pria/osiris/linker/implementation/hedgehog/axes/HorizontalAxis.java
+++ linker/src/main/java/at/pria/osiris/linker/implementation/hedgehog/axes/HorizontalAxis.java
@@ -1,15 +1,17 @@
 package at.pria.osiris.linker.implementation.hedgehog.axes;
 
 import at.pria.osiris.linker.controllers.components.Axes.MotorSensorAxis;
+import at.pria.osiris.linker.controllers.components.Axes.ServoAxis;
 import at.pria.osiris.linker.implementation.hedgehog.components.HedgehogMotor;
+import at.pria.osiris.linker.implementation.hedgehog.components.HedgehogServo;
 import org.andrix.low.NotConnectedException;
 
 /**
  * @author Ari Michael Ayvazyan
  * @version 16.02.2015
  */
-public class HorizontalAxis extends MotorSensorAxis {
+public class HorizontalAxis extends ServoAxis {
     public HorizontalAxis() throws NotConnectedException {
-        super("HorizontalAxis", new HedgehogMotor(2), null);
+        super("HorizontalAxis", new HedgehogServo(4));
     }
 }
--- linker/src/main/java/at/pria/osiris/linker/implementation/hedgehog/axes/VerticalAxis.java
+++ linker/src/main/java/at/pria/osiris/linker/implementation/hedgehog/axes/VerticalAxis.java
@@ -1,15 +1,41 @@
 package at.pria.osiris.linker.implementation.hedgehog.axes;
 
-import at.pria.osiris.linker.controllers.components.Axes.MotorSensorAxis;
-import at.pria.osiris.linker.implementation.hedgehog.components.HedgehogMotor;
+import at.pria.osiris.linker.controllers.components.Axes.Axis;
+import at.pria.osiris.linker.implementation.hedgehog.components.HedgehogServo;
 import org.andrix.low.NotConnectedException;
 
 /**
  * @author Ari Michael Ayvazyan
  * @version 16.02.2015
  */
-public class VerticalAxis extends MotorSensorAxis {
+public class VerticalAxis extends Axis {
+    private final HedgehogServo hedgehogServo2;
+    private final HedgehogServo hedgehogServo;
+    private int curentPosition;
+
     public VerticalAxis() throws NotConnectedException {
-        super("VerticalAxis", new HedgehogMotor(3), null);
+        super("VerticalAxis");
+        hedgehogServo = new HedgehogServo(2);
+        hedgehogServo2 = new HedgehogServo(3);
+    }
+
+    @Override
+    public int getSensorValue() {
+        return curentPosition;
+    }
+
+    @Override
+    public void moveToPosition(int position) {
+        curentPosition = position;
+        hedgehogServo.moveToAngle(position);
+        hedgehogServo2.moveToAngle(position);
+    }
+
+    @Override
+    public void moveAtPower(int power) {
+        //TODO Pfui :stinky:
+        curentPosition = power;
+        hedgehogServo.moveToAngle(power);
+        hedgehogServo2.moveToAngle(power);
     }
 }
