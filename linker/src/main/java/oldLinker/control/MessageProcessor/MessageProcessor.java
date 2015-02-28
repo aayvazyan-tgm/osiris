7234de7e364e01e22d218a972e9f7a9affeac195
Ari Ayvazyan <ariay@live.at>
2015-02-28 18:04:32 +0100

    removed the old linker, kept the stub

--- linker/src/main/java/oldLinker/control/MessageProcessor/MessageProcessor.java
+++
@@ -1,9 +0,0 @@
-package oldLinker.control.MessageProcessor;
-
-/**
- * @author Ari Ayvazyan
- * @version 03.Dec.14
- */
-public interface MessageProcessor {
-    abstract void processMessage(Object message);
-}
