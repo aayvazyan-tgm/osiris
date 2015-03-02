a05ce6e9bbabdc8a70a3d1bca7ba6b7d075fe55d
Ari Ayvazyan <ariay@live.at>
2015-03-02 16:58:21 +0100

    the wrong response type was sent

--- linker/src/main/java/at/pria/osiris/linker/communication/messageProcessors/AxisValueRequestProcessor.java
+++ linker/src/main/java/at/pria/osiris/linker/communication/messageProcessors/AxisValueRequestProcessor.java
@@ -2,6 +2,7 @@ package at.pria.osiris.linker.communication.messageProcessors;
 
 import at.pria.osiris.linker.controllers.RobotArm;
 import messages.requests.AxisValueRequest;
+import messages.responses.AxisValueResponse;
 import messages.responses.SensorValueResponse;
 
 /**
@@ -20,7 +21,7 @@ public class AxisValueRequestProcessor implements MessageProcessor {
         if (msg instanceof AxisValueRequest) {
             AxisValueRequest request = (AxisValueRequest) msg;
             int sensorVal = robotArm.getAxis(request.getAxisNumber()).getSensorValue();
-            SensorValueResponse response = new SensorValueResponse(request.getAxisNumber(), sensorVal);
+            AxisValueResponse response = new AxisValueResponse(request.getAxisNumber(), sensorVal);
             robotArm.getCommunicationInterface().sendMessage(response);
         }
     }
