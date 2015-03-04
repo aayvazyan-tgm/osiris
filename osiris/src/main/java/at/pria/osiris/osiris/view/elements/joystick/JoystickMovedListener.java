141b6e8d3ce6236aeed7d21746e1f89e8814e32a
Helmuth Brunner <hbrunner@student.tgm.ac.at>
2015-03-04 14:34:15 +0100

    line separator issue fix

--- osiris/src/main/java/at/pria/osiris/osiris/view/elements/joystick/JoystickMovedListener.java
+++ osiris/src/main/java/at/pria/osiris/osiris/view/elements/joystick/JoystickMovedListener.java
@@ -5,7 +5,7 @@ package at.pria.osiris.osiris.view.elements.joystick;
  * source: svn checkout http://mobile-anarchy-widgets.googlecode.com/svn/trunk/ mobile-anarchy-widgets-read-only
  */
 public interface JoystickMovedListener {
-	public void OnMoved(int pan, int tilt);
-	public void OnReleased();
-	public void OnReturnedToCenter();
+    public void OnMoved(int pan, int tilt);
+    public void OnReleased();
+    public void OnReturnedToCenter();
 }
