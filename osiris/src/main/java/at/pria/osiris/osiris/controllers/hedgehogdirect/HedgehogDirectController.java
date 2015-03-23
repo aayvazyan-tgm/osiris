0fd56d29fa9ce37c07a6d28ffcd521c47d444389
Ari Ayvazyan <ariay@live.at>
2015-03-23 15:30:57 +0100

    started to implement the direct communication

--- osiris/src/main/java/at/pria/osiris/osiris/controllers/hedgehogdirect/HedgehogDirectController.java
+++ osiris/src/main/java/at/pria/osiris/osiris/controllers/hedgehogdirect/HedgehogDirectController.java
@@ -6,6 +6,7 @@ import api.Robotarm;
 import at.pria.osiris.osiris.controllers.ConnectionNotEstablishedException;
 import at.pria.osiris.osiris.controllers.Controller;
 import at.pria.osiris.osiris.controllers.ControllerSetup;
+import at.pria.osiris.osiris.controllers.hedgehog.HedgehogSetup;
 import at.pria.osiris.osiris.controllers.hedgehog.OldLinkerHedgehogRemoteRobotarm;
 
 /**
@@ -17,7 +18,7 @@ public class HedgehogDirectController implements Controller {
     private ControllerSetup hedgehogDirectSetup;
 //    private CommunicationClassWithSendDataMethod something;
 
-    public HedgehogDirectController(/*Configuration*/) { this.hedgehogDirectSetup = new HedgehogDirectSetup(); }
+    public HedgehogDirectController(/*Configuration*/) { this.hedgehogDirectSetup = new HedgehogSetup(); }
 
     @Override
     public ControllerSetup getSetup() {
@@ -27,7 +28,7 @@ public class HedgehogDirectController implements Controller {
     @Override
     public Robotarm getRobotArm() throws ConnectionNotEstablishedException {
         try {
-            return OldLinkerHedgehogRemoteRobotarm.getInstance();
+            return DirectCommunicationRobotArm.getInstance();
         } catch (IOException e) {
             throw new ConnectionNotEstablishedException(e);
         }
