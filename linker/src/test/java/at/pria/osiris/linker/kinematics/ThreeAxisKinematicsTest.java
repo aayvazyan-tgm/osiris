bc28f99c3e65688de92907c96aba7247c3236482
abergler-tgm <adrian.bergler@student.tgm.ac.at>
2015-03-24 18:42:47 +0100

    Started implementing the Kinematics and removed old kinematic stubs

--- linker/src/test/java/at/pria/osiris/linker/kinematics/ThreeAxisKinematicsTest.java
+++ linker/src/test/java/at/pria/osiris/linker/kinematics/ThreeAxisKinematicsTest.java
@@ -1,13 +1,21 @@
 package at.pria.osiris.linker.kinematics;
 
+import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 /**
  * @author Adrian Bergler
  * @version 2015-03-23
  */
 public class ThreeAxisKinematicsTest {
+
+    private static Logger logger = Logger.getLogger(ThreeAxisKinematicsTest.class);
+
+    private double[] fragmentlengths = {26, 23};
+    private double[][] padding = {{0,0,0},{0,0,0},{0,0,0}};
 
     @Before
     public void before() throws Exception {
@@ -19,6 +27,12 @@ public class ThreeAxisKinematicsTest {
      */
     @Test
     public void testP1(){
+        Kinematic kinematics =
+                new ThreeAxisKinematics(32, 0, 24, fragmentlengths, padding);
+
+        ArrayList<Double> solution = kinematics.calculateValues();
+
+        System.out.println(solution.toString());
 
     }
 
